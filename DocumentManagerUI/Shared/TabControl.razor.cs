1a858db32dfd4bc8e4a384a67fd7a29969fa418d
dragonslayp@mail.ru <dragonslayp@mail.ru>
2022-04-05 22:48:01 +0300

    Добавьте файлы проекта.

---
+++ DocumentManagerUI/Shared/TabControl.razor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Microsoft.JSInterop;
+using DocumentManagerUI;
+using DocumentManagerUI.Pages;
+using DocumentManagerUI.Shared;
+using Blazorise;
+using ModelDomain;
+
+
+namespace DocumentManagerUI.Shared
+{
+    public partial class TabControl
+    {
+        public TabPage ActivePage { get; set; }
+        List<TabPage> Pages = new List<TabPage>();
+        internal void AddPage(TabPage tabPage)
+        {
+            Pages.Add(tabPage);
+            if (Pages.Count == 1)
+                ActivePage = tabPage;
+            StateHasChanged();
+        }
+
+        string GetButtonClass(TabPage page)
+        {
+            return page == ActivePage ? "buttonClass barButtomActive" : "buttonClass barButtom";
+        }
+
+        void ActivatePage(TabPage page)
+        {
+            ActivePage = page;
+        }
+
+        IconName IconMade(string name)
+        {
+            if (name == "Files")
+            {
+                return IconName.FileAlt;
+            }
+            else if (name == "Documents")
+            {
+                return IconName.Folder;
+            }
+            else if (name == "Pictures")
+            {
+                return IconName.TicketAlt;
+            }
+            else return IconName.Video;
+        }
+    }
+}
