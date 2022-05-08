4c3b29c2be9f37b33c7e6deb451d374ca278ca43
dragonslayp@mail.ru <dragonslayp@mail.ru>
2022-05-08 22:29:34 +0300

    baguvix

---
+++ DocumentManagerUI/ErrorResponses/ResponseFile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+
+namespace DocumentManager.API.ResponseFile
+{
+    public class ResponseFile
+    {
+        public string ObjectId { get; }
+        public byte[] Content { get; }
+
+        public ResponseFile(string objectId, byte[] content)
+        {
+            ObjectId = objectId;
+            Content = content;
+        }
+    }
+}
