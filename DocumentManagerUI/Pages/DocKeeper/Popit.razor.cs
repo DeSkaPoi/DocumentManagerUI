ad4ad5735d9012a10dde6e24366a46b46894a05f
dragonslayp@mail.ru <dragonslayp@mail.ru>
2022-05-29 15:48:07 +0300

    ref

---
+++ DocumentManagerUI/Pages/DocKeeper/Popit.razor.cs
@@ -0,0 +1,14 @@
+﻿namespace DocumentManagerUI.Pages.DocKeeper
+{
+    public class Popitis
+    {
+        public string File { get; }
+        public string Name { get; }
+
+        public Popitis(string file, string name)
+        {
+            File = file;
+            Name = name;
+        }
+    }
+}
