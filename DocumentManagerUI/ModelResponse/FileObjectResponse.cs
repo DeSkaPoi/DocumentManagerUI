791a0db0bacce7a0b7067ad7ee6af3eae62c8e06
dragonslayp@mail.ru <dragonslayp@mail.ru>
2022-09-04 22:11:42 +0300

    2/0

--- DocumentManagerUI/ModelResponse/FileObjectResponse.cs
+++ DocumentManagerUI/ModelResponse/FileObjectResponse.cs
@@ -4,14 +4,16 @@ namespace DocumentManagerUI.ModelResponse
 {
     public class FileObjectResponse
     {
-        public Guid Id { get; set; }
         public string Name { get; set; }
         public byte[] File { get; set; }
         public string FileTypeMime { get; set; }
 
-        public FileObjectResponse(Guid id, string name, byte[] file, string fileTypeMime)
+        public FileObjectResponse()
         {
-            Id = id;
+        }
+
+        public FileObjectResponse(string name, byte[] file, string fileTypeMime)
+        {
             Name = name;
             File = file;
             FileTypeMime = fileTypeMime;
